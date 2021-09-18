bfd74b8ad21d16b432ee395314c39b822f7873a9
andrzejbajuk79 <ptasiek3003@gmail.com>
2021-09-18 11:24:31 +0200

    Masonry

--- src/components/home/HomePosts.jsx
+++ src/components/home/HomePosts.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import Masonry from "react-masonry-css";
+import Moment from "react-moment";
+import { LinkContainer } from "react-router-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { getPosts } from "../../store/actions";
 import { Spinner, Button } from "react-bootstrap";
@@ -21,6 +24,35 @@ const HomePosts = () => {
 
 	return (
 		<>
+			<Masonry
+				breakpointCols={{ default: 3, 800: 2, 400: 1 }}
+				className='my-masonry-grid'
+				columnClassName='my-masonry-grid_column'
+			>
+				{homePosts.posts
+					? homePosts.posts.map((item) => (
+							<div key={item.id}>
+								<img
+									style={{ width: "100%", height: "200px" }}
+									src={item.image}
+								/>
+								<div className='author'>
+									<span>{item.author} - </span>
+									<Moment format='DD MMMM'>{item.createdAt}</Moment>
+								</div>
+								<div className='content'>
+									<div className='title'>{item.title}</div>
+									<div className='excerpt'>{item.excerpt}</div>
+									<LinkContainer to={`/article/${item.id}`} className='mt-3'>
+										<Button onClick={() => {}} variant='light'>
+											Read more
+										</Button>
+									</LinkContainer>
+								</div>
+							</div>
+					  ))
+					: null}
+			</Masonry>
 			{loading ? (
 				<div style={{ textAlign: "center" }}>
 					<Spinner animation='border' role='status'>
